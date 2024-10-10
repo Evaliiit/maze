cad1739dd71465751ea5409547c488d8136106ac
Evaliiit <354323@viauc.dk>
2024-10-10 18:22:01 +0200

    Initial files for MakeCode project

---
+++ tilemap.g.ts
@@ -0,0 +1,58 @@
+// Auto-generated code. Do not edit.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tilemap", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "level1":
+            case "level1":return tiles.createTilemap(hex`1000100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, [myTiles.transparency16], TileScale.Sixteen);
+            case "level2":
+            case "level2":return tiles.createTilemap(hex`1000100018180808080808080808080808080807051818181818090d1418181818180904050e180e0c181816151818180f180904010918180f181818181818090f180904011818090f1810180e0e0e0e0b1818040118100a0b1811091818181818181804011811091818120e0e0e0e180c0d14040109111818181818091818180f1615040118120a180c18100e0c18180f09170401181818180f1818180f09180f1818040118100a0a0b180f180f18180f180904011813091818180f180f09180f181804011818181818180f180f1818181818040518120a0a0a180b1812180a0b0d14040118181919181819181918181816150402030303030303030303030303030306`, img`
+. . 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 . . . . . 2 2 2 . . . . . 2 2 
+2 2 . 2 2 . . 2 2 . . . 2 . 2 2 
+2 2 . . 2 . . . . . . 2 2 . 2 2 
+2 . . 2 2 . 2 . 2 2 2 2 2 . . 2 
+2 . 2 2 2 . 2 2 . . . . . . . 2 
+2 . 2 2 . . 2 2 2 2 2 . 2 2 2 2 
+2 2 2 . . . . . 2 . . . 2 2 2 2 
+2 . 2 2 . 2 . 2 2 2 . . 2 2 . 2 
+2 . . . . 2 . . . 2 2 . 2 . . 2 
+2 . 2 2 2 2 . 2 . 2 . . 2 . 2 2 
+2 . 2 2 . . . 2 . 2 2 . 2 . . 2 
+2 . . . . . . 2 . 2 . . . . . 2 
+2 . 2 2 2 2 . 2 . 2 . 2 2 2 2 2 
+2 . . 2 2 . . 2 . 2 . . . 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+`, [myTiles.transparency16,sprites.dungeon.greenOuterWest0,sprites.dungeon.greenOuterSouthEast,sprites.dungeon.greenOuterSouth0,sprites.dungeon.greenOuterEast0,sprites.dungeon.greenOuterWest1,sprites.dungeon.greenOuterSouthWest,sprites.dungeon.greenOuterNorthEast,sprites.dungeon.greenOuterNorth0,sprites.dungeon.greenOuterEast2,sprites.dungeon.purpleOuterSouth1,sprites.dungeon.purpleOuterSouthWest,sprites.dungeon.purpleOuterNorthEast,sprites.dungeon.purpleInnerNorthWest,sprites.dungeon.purpleOuterNorth1,sprites.dungeon.purpleOuterEast0,sprites.dungeon.purpleOuterNorthWest,sprites.dungeon.purpleOuterWest0,sprites.dungeon.purpleOuterSouthEast,sprites.dungeon.purpleOuterWest1,sprites.dungeon.purpleInnerNorthEast,sprites.dungeon.purpleInnerSouthEast,sprites.dungeon.purpleInnerSouthWest,sprites.dungeon.chestClosed,sprites.dungeon.floorDark0,sprites.dungeon.greenOuterNorth2], TileScale.Sixteen);
+        }
+        return null;
+    })
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "transparency16":return transparency16;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
